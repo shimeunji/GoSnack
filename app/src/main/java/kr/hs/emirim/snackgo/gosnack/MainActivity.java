eaf090004a76bacf19f5be0859f04d22b5b24456
Eunji Shim <shimeunji991222@gmail.com>
2016-06-12 03:14:14 +0900

    #4 Add ImageButton

--- app/src/main/java/kr/hs/emirim/snackgo/gosnack/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/snackgo/gosnack/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.snackgo.gosnack;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.buttontest.R;
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Button rank_but = (Button) findViewById(R.id.but_rank);
         Button lab_but = (Button) findViewById(R.id.but_lab);
         Button random_but = (Button) findViewById(R.id.but_random);
+        ImageButton link_imgbut=(ImageButton)findViewById(R.id.img_link);
 
 
         rank_but.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent3);
             }
         });
+        link_imgbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/snackgwaza/?fref=ts"));
+                startActivity(intent);
+            }
+        });
 
     }
     public boolean onCreateOptionsMenu(Menu menu)
@@ -48,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateOptionsMenu(menu);
 
         MenuItem item1=menu.add(0,1,0,"설정");
+        MenuItem item2=menu.add(0,2,0,"my Page");
         return true;
     }
 }
